ccc0af517752fe06c8162aa8ed7098375ea2113d
litisrael <isroellit@gmail.com>
2023-01-18 15:26:01 +0200

    traar de subir

---
+++ animal-data/index.js
@@ -0,0 +1,23 @@
+import Client from 'pg/lib/client.js'
+
+async function makeQuery(animal_class, diet, behavior, age ,lifespan)
+
+
+{const client = new Client({
+        user: 'animal_server',
+        password: '01456',
+        database: "animals"
+      })
+      await client.connect()
+    
+      const res = await client.query(
+        `INSERT INTO public.animals(animal_class, diet, behavior, age ,lifespan)
+        VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
+       [animal_class, diet, behavior, age ,lifespan]
+       
+      )
+      console.log("%j", res.rows)
+      await client.end()
+}
+
+      makeQuery("pato","pan","bla",'5','10')
